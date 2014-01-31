3e8631682e290ec1a4a45f57d1d0c0eb8e6932a4
Jacob Lifshay <programmerjake@gmail.com>
2014-01-30 22:25:21 -0800

    add thread pool

--- src/test.cpp
+++ src/test.cpp
@@ -6,22 +6,30 @@
 #include <atomic>
 #include <cstdio>
 #include <ctime>
+#include <vector>
+#include <mutex>
+#include <condition_variable>
 
 using namespace std;
 using namespace PathTrace;
 const bool multiThreaded = true;
-const int rendererCount = ([](int cpuCount)
-{
-    return cpuCount == 0 ? 4000 : cpuCount * 8;
-})(thread::hardware_concurrency());
+const int rendererCount = 4000;
 const int rayCount = 100000;
-const int rayDepth = 64;
+const int rayDepth = 4;//64;
 const int ScreenWidth = 1680, ScreenHeight = 1050;
-const char * const ProgramName = "Path Trace Test";
-const float minimumColorDelta = 0.0015; // if the color change is less than this then we don't need to check inside this box
-const int blockSize = [](int count){int retval=1024;while(retval>count&&retval>4)retval/=2;return retval/4;}(ScreenWidth/4), maximumSampleSize = ScreenHeight / (480 / 8);
+const char *const ProgramName = "Path Trace Test";
+const float minimumColorDelta = 1 / pow((float)rayCount / 2, 0.4); // if the color change is less than this then we don't need to check inside this box
+const int blockSize = [](int count)
+{
+    int retval = 1024;
+    while(retval > count && retval > 4)
+    {
+        retval /= 2;
+    }
+    return retval / 4;
+}(ScreenWidth / 4), maximumSampleSize = ScreenHeight / (480 / 16);
 
-Object * unionArray(Object * array[], int start, int end)
+Object *unionArray(Object *array[], int start, int end)
 {
     if(end - start == 1)
     {
@@ -35,7 +43,7 @@ Object * unionArray(Object * array[], int start, int end)
     return new Union(unionArray(array, start, split), unionArray(array, split, end));
 }
 
-Object * makeLens(Vector3D position, Vector3D orientation, float radius, float sphereRadius, const Material * material)
+Object *makeLens(Vector3D position, Vector3D orientation, float radius, float sphereRadius, const Material *material)
 {
     assert(radius <= sphereRadius);
     float dist = sqrt(sphereRadius * sphereRadius - radius * radius);
@@ -43,62 +51,198 @@ Object * makeLens(Vector3D position, Vector3D orientation, float radius, float s
     return new Intersection(new Sphere(position + orientation * dist, sphereRadius, material), new Sphere(position - orientation * dist, sphereRadius, material));
 }
 
-Object * makeLens(Vector3D position, Vector3D focus, float radius, const Material * material)
+Object *makeLensPointedAt(Vector3D position, Vector3D focus, float focusFactor, float radius, const Material *material)
 {
     float ior = material->ior;
     assert(ior > 1 + eps);
-    float distance = abs(focus - position);
+    float distance = abs(focus - position) * focusFactor;
     assert(distance > eps);
     return makeLens(position, focus - position, radius, 2 * distance * (ior - 1), material);
 }
 
-Object * makeWorld()
+Vector3D interpolate(float t, Vector3D a, Vector3D b)
 {
-    static Material matEmitR = Material(Color(0, 0, 0), 0, Color(6, 0, 0));
-    static Material matEmitG = Material(Color(0, 0, 0), 0, Color(0, 6, 0));
-    static Material matEmitB = Material(Color(0, 0, 0), 0, Color(0, 0, 6));
+    return a + t * (b - a);
+}
+
+Object *makeWorld()
+{
+    static Material matEmitR = Material(Color(0, 0, 0), 0, Color(12, 0, 0));
+    static Material matEmitG = Material(Color(0, 0, 0), 0, Color(0, 12, 0));
+    static Material matEmitB = Material(Color(0, 0, 0), 0, Color(0, 0, 12));
     static Material matEmitW = Material(Color(0, 0, 0), 0, Color(2, 2, 2));
-    static Material matEmitBrightW = Material(Color(0, 0, 0), 0, Color(40, 40, 40));
+    static Material matEmitBrightW = Material(Color(0, 0, 0), 0, Color(80, 80, 80));
     static Material matDiffuseWhite = Material(Color(0.8, 0.8, 0.8), 1);
     static Material matGlass = Material(Color(0.7, 0.7, 0.7), 0, Color(0, 0, 0), Color(0.9, 0.9, 0.9), 1.3, 1);
     static Material matDiamond = Material(Color(0.9, 0.9, 0.9), 0, Color(0, 0, 0), Color(0.95, 0.95, 0.95), 2.419, 1);
     static Material matMirror = Material(Color(0.99, 0.99, 0.99), 0);
-    Object * objects[] =
+    Object *objects[] =
     {
-        new Sphere(Vector3D(1, 16, -3), 6, &matEmitR),
-        new Sphere(Vector3D(0, 16, -2), 6, &matEmitG),
-        new Sphere(Vector3D(-1, 16, -3), 6, &matEmitB),
-        new Sphere(Vector3D(0, 14, -3), 4, &matEmitBrightW),
+        new Sphere(Vector3D(-1 + sin(M_PI * 2 / 3) * 3, 6 + cos(M_PI * 2 / 3) * 3, 14), 6, &matEmitR),
+        new Sphere(Vector3D(-1 + sin(M_PI * 4 / 3) * 3, 6 + cos(M_PI * 2 / 3) * 3, 14), 6, &matEmitB),
+        new Sphere(Vector3D(-1, 6 + 3, 14), 6, &matEmitG),
+        new Sphere(Vector3D(-1, 6, 14), 6, &matEmitBrightW),
+        new Sphere(Vector3D(-1, 6, 16), 7.5, &matMirror),
         new Sphere(Vector3D(1, 0, -4), 0.2, &matDiffuseWhite),
         new Sphere(Vector3D(-1, 0, -4), 0.2, &matDiffuseWhite),
-        new Plane(Vector3D(0, 0, -1), 20, &matDiffuseWhite),
+        new Plane(Vector3D(0, 0, -1), 10, &matDiffuseWhite),
         new Plane(Vector3D(0, 0, 1), 20, &matDiffuseWhite),
         new Plane(Vector3D(0, -1, 0), 20, &matEmitW),
-        new Plane(Vector3D(0, 1, 0), 1, &matDiffuseWhite),
+        new Plane(Vector3D(0, 1, 0), 5, &matDiffuseWhite),
         new Plane(Vector3D(-1, 0, 0), 20, &matDiffuseWhite),
         new Plane(Vector3D(1, 0, 0), 20, &matDiffuseWhite),
-        makeLens(Vector3D(-3.0 / 4, 0, -3), Vector3D(-1, 0, -3), 0.5, 1, &matGlass),
-        makeLens(Vector3D(0, 1.5, -5), Vector3D(0, -1, -5), 0.8, &matDiamond),
+        //makeLens(Vector3D(-2.5 / 4, 0, -2.5), Vector3D(-1, 0, -4), 0.5, 1, &matGlass),
+        makeLensPointedAt(interpolate(0.9, Vector3D(-1, 10, 14), Vector3D(0, 0, -10)), Vector3D(0, -1, -20), 1.2, 2.5, &matDiamond),
     };
     return unionArray(objects, 0, sizeof(objects) / sizeof(objects[0]));
 }
 
-namespace
+class Runnable
 {
-class RenderBlock
+private:
+    static thread **threads;
+    static atomic_bool *threadDone;
+    static int threadCount;
+    static Runnable *head, *tail;
+    static bool didInit;
+    static mutex listMutex;
+    Runnable *next, *prev;
+    atomic_bool started, finished;
+    condition_variable_any finished_cond;
+
+    static void threadFn(atomic_bool * done)
+    {
+        listMutex.lock();
+        while(head)
+        {
+            Runnable *theRunner = head;
+            head = head->next;
+            if(head)
+                head->prev = NULL;
+            else
+                tail = NULL;
+            theRunner->prev = NULL;
+            theRunner->next = NULL;
+            theRunner->started = true;
+            listMutex.unlock();
+            try
+            {
+                theRunner->run();
+            }
+            catch(...)
+            {
+                exit(1);
+            }
+            listMutex.lock();
+            theRunner->finished = true;
+            theRunner->finished_cond.notify_all();
+        }
+        *done = true;
+        listMutex.unlock();
+    }
+
+    static void doInit()
+    {
+        if(didInit)
+        {
+            return;
+        }
+        didInit = true;
+        threadCount = thread::hardware_concurrency();
+        if(threadCount == 0)
+        {
+            threadCount = 4;
+        }
+        threads = new thread *[threadCount];
+        threadDone = new atomic_bool[threadCount];
+        for(int i = 0; i < threadCount; i++)
+        {
+            threads[i] = NULL;
+            threadDone[i] = true;
+        }
+    }
+    Runnable(const Runnable &) = delete;
+    const Runnable &operator =(const Runnable &) = delete;
+public:
+    Runnable()
+    {
+        doInit();
+    }
+    void start()
+    {
+        listMutex.lock();
+        if(head)
+            tail->next = this;
+        else
+            head = this;
+        next = NULL;
+        prev = tail;
+        tail = this;
+        listMutex.unlock();
+        for(int i = 0; i < threadCount; i++)
+        {
+            if(threadDone[i])
+            {
+                if(threads[i])
+                {
+                    threads[i]->join();
+                }
+                delete threads[i];
+                threadDone[i] = false;
+                threads[i] = new thread(threadFn, &threadDone[i]);
+                break;
+            }
+        }
+    }
+    virtual void run() = 0;
+    void join()
+    {
+        listMutex.lock();
+        while(!finished)
+            finished_cond.wait(listMutex);
+        listMutex.unlock();
+    }
+    virtual ~Runnable()
+    {
+        listMutex.lock();
+        if(started)
+        {
+            while(!finished)
+                finished_cond.wait(listMutex);
+        }
+        else
+        {
+            if(next == NULL)
+                tail = prev;
+            else
+                next->prev = prev;
+            if(prev == NULL)
+                head = next;
+            else
+                prev->next = next;
+        }
+        listMutex.unlock();
+    }
+};
+
+thread **Runnable::threads = NULL;
+atomic_bool *Runnable::threadDone = NULL;
+int Runnable::threadCount = 0;
+bool Runnable::didInit = false;
+Runnable *Runnable::head = NULL, *Runnable::tail = NULL;
+mutex Runnable::listMutex;
+
+class RenderBlock final : public Runnable
 {
 public:
-    RenderBlock(const int x, const int y, int size, const Object * world)
-        : xOrigin(x), yOrigin(y), buffer(new Color[(size + 1) * (size + 1)]), validBuffer(new bool[(size + 1) * (size + 1)]), size(size), world(world), ran_finish(false), finished(false)
+    RenderBlock(const int x, const int y, int size, const Object *world)
+        : Runnable(), xOrigin(x), yOrigin(y), buffer(new Color[(size + 1) * (size + 1)]), validBuffer(new bool[(size + 1) * (size + 1)]), size(size), world(world), ran_finish(false), finished(false)
     {
         for(int i = 0; i < (size + 1) * (size + 1); i++)
         {
             validBuffer[i] = false;
         }
-        if(multiThreaded)
-        {
-            theThread = new thread(runFn, this);
-        }
+        start();
     }
     void finish()
     {
@@ -106,14 +250,7 @@ public:
         {
             return;
         }
-        if(multiThreaded)
-        {
-            theThread->join();
-        }
-        else
-        {
-            run();
-        }
+        join();
         ran_finish = true;
     }
     bool done()
@@ -128,7 +265,7 @@ public:
         }
         return false;
     }
-    void copyToBuffer(Color * screenBuffer, int w, int h)
+    void copyToBuffer(Color *screenBuffer, int w, int h)
     {
         for(int y = yOrigin; y < yOrigin + size && y < h; y++)
         {
@@ -143,20 +280,15 @@ public:
     }
     ~RenderBlock()
     {
-        if(multiThreaded)
-        {
-            finish();
-        }
         delete []buffer;
         delete []validBuffer;
-        delete theThread;
     }
 private:
-    Color & pixel(int x, int y)
+    Color &pixel(int x, int y)
     {
         return buffer[x - xOrigin + (y - yOrigin) * (size + 1)];
     }
-    bool & validPixel(int x, int y)
+    bool &validPixel(int x, int y)
     {
         return validBuffer[x - xOrigin + (y - yOrigin) * (size + 1)];
     }
@@ -200,14 +332,26 @@ private:
                 return pixel(x, y);
             }
         }
-        Color retval = tracePixel(*spanIterator, x, y, ScreenWidth, ScreenHeight, rayCount, rayDepth, ScreenWidth, ScreenHeight, min(ScreenWidth, ScreenHeight));
+        Color retval = tracePixel(*spanIterator, x, y, ScreenWidth, ScreenHeight, rayCount, rayDepth, ScreenWidth, ScreenHeight, min(ScreenWidth, ScreenHeight) * 2);
+        float r = max(retval.x, max(retval.y, retval.z));
+        if(r > 1)
+        {
+            retval.x += (r - 1) / 2;
+            retval.y += (r - 1) / 2;
+            retval.z += (r - 1) / 2;
+            retval.x = min(1.0f, retval.x);
+            retval.y = min(1.0f, retval.y);
+            retval.z = min(1.0f, retval.z);
+        }
         setPixel(x, y, retval);
         return retval;
     }
     void renderSquare(int x, int y, int size, Color tl, Color tr, Color bl, Color br)
     {
         if(x > ScreenWidth || y > ScreenHeight)
+        {
             return;
+        }
         if(size <= 1)
         {
             setPixel(x, y, tl);
@@ -236,30 +380,26 @@ private:
         renderSquare(x, cy, halfSize, cl, cc, bl, bc);
         renderSquare(cx, cy, halfSize, cc, cr, bc, br);
     }
-    void run() throw()
+protected:
+    virtual void run() override
     {
         spanIterator = world->makeSpanIterator();
         renderSquare(xOrigin, yOrigin, size, calcPixelColor(xOrigin, yOrigin), calcPixelColor(xOrigin + size, yOrigin), calcPixelColor(xOrigin, yOrigin + size), calcPixelColor(xOrigin + size, yOrigin + size));
         delete spanIterator;
         finished = true;
     }
-    static void runFn(RenderBlock * data)
-    {
-        data->run();
-    }
+private:
     const int xOrigin, yOrigin;
-    thread * theThread;
-    Color * const buffer;
-    bool * const validBuffer;
+    Color *const buffer;
+    bool *const validBuffer;
     const int size;
-    const Object * world;
+    const Object *world;
     bool ran_finish;
-    SpanIterator * spanIterator;
+    SpanIterator *spanIterator;
     atomic_bool finished;
 };
-}
 
-int main(int argc, char ** argv)
+int main(int argc, char **argv)
 {
     if(argc == 2)
     {
@@ -271,20 +411,26 @@ int main(int argc, char ** argv)
     }
     bool useVideo = true;
     if(argc >= 2 && string(argv[1]) == "--novideo")
+    {
         useVideo = false;
+    }
     if(SDL_Init(useVideo ? SDL_INIT_VIDEO : 0) != 0)
     {
         cerr << "\nUnable to initialize SDL:  " << SDL_GetError() << endl;
         return EXIT_FAILURE;
     }
     atexit(SDL_Quit);
-    SDL_Surface * screen = NULL;
+    SDL_Surface *screen = NULL;
     if(useVideo)
+    {
         screen = SDL_SetVideoMode(ScreenWidth, ScreenHeight, 32, SDL_SWSURFACE);
+    }
     if(screen == NULL)
     {
         if(useVideo)
+        {
             cerr << "\nUnable to set video mode:  " << SDL_GetError() << endl;
+        }
         useVideo = false;
         screen = SDL_CreateRGBSurface(SDL_SWSURFACE, ScreenWidth, ScreenHeight, 32, 0xFF0000, 0xFF00, 0xFF, 0xFF000000);
         if(screen == NULL)
@@ -298,16 +444,16 @@ int main(int argc, char ** argv)
     bool rendered = false;
     int bx = 0, by = 0;
     int count = 1;
-    Color * screenBuffer = new Color[ScreenWidth * ScreenHeight];
+    Color *screenBuffer = new Color[ScreenWidth * ScreenHeight];
     while(SDL_LockSurface(screen) != 0)
         ;
     for(int y = 0; y < ScreenHeight; y++)
     {
         for(int x = 0; x < ScreenWidth; x++)
         {
-            int r = 0x40;
+            int r = 0x80;
             int g = 0x0;
-            int b = 0x40;
+            int b = 0x80;
             screenBuffer[x + y * ScreenWidth] = Color(r / (float)0xFF, g / (float)0xFF, b / (float)0xFF);
             *(Uint32 *)((Uint8 *)screen->pixels + y * screen->pitch + x * screen->format->BytesPerPixel) = SDL_MapRGB(screen->format, r, g, b);
         }
@@ -318,9 +464,9 @@ int main(int argc, char ** argv)
         SDL_Flip(screen);
         SDL_WM_SetCaption(ProgramName, NULL);
     }
-    Object * const world = makeWorld();
+    Object *const world = makeWorld();
     AutoDestruct<Object> autoDestruct1(world);
-    RenderBlock * renderers[rendererCount];
+    RenderBlock *renderers[rendererCount];
     bool renderedAllBlocks = true;
     while(!done)
     {
@@ -330,7 +476,9 @@ int main(int argc, char ** argv)
             if(!useVideo)
             {
                 if(rendered)
+                {
                     done = true;
+                }
                 break;
             }
             if(rendered)
@@ -414,17 +562,17 @@ int main(int argc, char ** argv)
                         if(renderers[i] != nullptr)
                         {
                             renderers[i]->copyToBuffer(screenBuffer, ScreenWidth, ScreenHeight);
-                                for(int y = tempY; y < tempY + blockSize && y < ScreenHeight; y++)
+                            for(int y = tempY; y < tempY + blockSize && y < ScreenHeight; y++)
+                            {
+                                for(int x = tempX; x < tempX + blockSize && x < ScreenWidth; x++)
                                 {
-                                    for(int x = tempX; x < tempX + blockSize && x < ScreenWidth; x++)
-                                    {
-                                        Color & c = screenBuffer[x + ScreenWidth * y];
-                                        int r = max(0, min(0xFF, (int)floor(0x100 * c.x / count)));
-                                        int g = max(0, min(0xFF, (int)floor(0x100 * c.y / count)));
-                                        int b = max(0, min(0xFF, (int)floor(0x100 * c.z / count)));
-                                        *(Uint32 *)((Uint8 *)screen->pixels + y * screen->pitch + x * screen->format->BytesPerPixel) = SDL_MapRGB(screen->format, r, g, b);
-                                    }
+                                    Color &c = screenBuffer[x + ScreenWidth * y];
+                                    int r = max(0, min(0xFF, (int)floor(0x100 * c.x / count)));
+                                    int g = max(0, min(0xFF, (int)floor(0x100 * c.y / count)));
+                                    int b = max(0, min(0xFF, (int)floor(0x100 * c.z / count)));
+                                    *(Uint32 *)((Uint8 *)screen->pixels + y * screen->pitch + x * screen->format->BytesPerPixel) = SDL_MapRGB(screen->format, r, g, b);
                                 }
+                            }
                         }
                         tempX += blockSize;
                         if(tempX >= ScreenWidth)
@@ -465,16 +613,16 @@ int main(int argc, char ** argv)
                         continue;
                     }
                     renderers[i]->copyToBuffer(screenBuffer, ScreenWidth, ScreenHeight);
-                        for(int y = by; y < by + blockSize && y < ScreenHeight; y++)
+                    for(int y = by; y < by + blockSize && y < ScreenHeight; y++)
+                    {
+                        for(int x = bx; x < bx + blockSize && x < ScreenWidth; x++)
                         {
-                            for(int x = bx; x < bx + blockSize && x < ScreenWidth; x++)
-                            {
-                                Color & c = screenBuffer[x + ScreenWidth * y];
-                                int r = max(0, min(0xFF, (int)floor(0x100 * c.x / count)));
-                                int g = max(0, min(0xFF, (int)floor(0x100 * c.y / count)));
-                                int b = max(0, min(0xFF, (int)floor(0x100 * c.z / count)));
-                                *(Uint32 *)((Uint8 *)screen->pixels + y * screen->pitch + x * screen->format->BytesPerPixel) = SDL_MapRGB(screen->format, r, g, b);
-                            }
+                            Color &c = screenBuffer[x + ScreenWidth * y];
+                            int r = max(0, min(0xFF, (int)floor(0x100 * c.x / count)));
+                            int g = max(0, min(0xFF, (int)floor(0x100 * c.y / count)));
+                            int b = max(0, min(0xFF, (int)floor(0x100 * c.z / count)));
+                            *(Uint32 *)((Uint8 *)screen->pixels + y * screen->pitch + x * screen->format->BytesPerPixel) = SDL_MapRGB(screen->format, r, g, b);
+                        }
                     }
                     delete renderers[i];
                     bx += blockSize;
@@ -502,7 +650,9 @@ int main(int argc, char ** argv)
                 static int dotCount = 0;
                 cout << "rendering.";
                 for(int i = dotCount++; i >= 0; i++)
+                {
                     cout << ".";
+                }
                 cout << "\x1B[K\r";
                 dotCount %= 15;
             }
