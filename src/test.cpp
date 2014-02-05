36ca4b54a3bbeba647051aac5fd6a248bcd9643c
Jacob Lifshay <programmerjake@gmail.com>
2014-02-05 12:28:22 -0800

    add network support

--- src/test.cpp
+++ src/test.cpp
@@ -8,17 +8,25 @@
 #include <ctime>
 #include <vector>
 #include <mutex>
+#include <sstream>
 #include <condition_variable>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netdb.h>
+#include <unistd.h>
+#include <cstring>
+#include <signal.h>
 
 using namespace std;
 using namespace PathTrace;
+const char * NET_PORT = "12346";
 const bool multiThreaded = true;
-const int rendererCount = 4000;
-const int rayCount = 100000;
-const int rayDepth = 4;//64;
-const int ScreenWidth = 1680, ScreenHeight = 1050;
+const int rendererCount = 1000;
+const int rayCount = 1000;
+const int rayDepth = 16;
+const int ScreenWidth = 1920 / 10, ScreenHeight = 1080 / 10;
 const char *const ProgramName = "Path Trace Test";
-const float minimumColorDelta = 1 / pow((float)rayCount / 2, 0.4); // if the color change is less than this then we don't need to check inside this box
+const float minimumColorDelta = 1 / 300.0; // if the color change is less than this then we don't need to check inside this box
 const int blockSize = [](int count)
 {
     int retval = 1024;
@@ -27,7 +35,7 @@ const int blockSize = [](int count)
         retval /= 2;
     }
     return retval / 4;
-}(ScreenWidth / 4), maximumSampleSize = ScreenHeight / (480 / 16);
+}(ScreenWidth / 2), maximumSampleSize = ScreenHeight / (480 / 8);
 
 Object *unionArray(Object *array[], int start, int end)
 {
@@ -67,14 +75,14 @@ Vector3D interpolate(float t, Vector3D a, Vector3D b)
 
 Object *makeWorld()
 {
-    static Material matEmitR = Material(Color(0, 0, 0), 0, Color(12, 0, 0));
-    static Material matEmitG = Material(Color(0, 0, 0), 0, Color(0, 12, 0));
-    static Material matEmitB = Material(Color(0, 0, 0), 0, Color(0, 0, 12));
+    static Material matEmitR = Material(Color(0, 0, 0), 0, Color(24, 0, 0));
+    static Material matEmitG = Material(Color(0, 0, 0), 0, Color(0, 24, 0));
+    static Material matEmitB = Material(Color(0, 0, 0), 0, Color(0, 0, 24));
     static Material matEmitW = Material(Color(0, 0, 0), 0, Color(2, 2, 2));
-    static Material matEmitBrightW = Material(Color(0, 0, 0), 0, Color(80, 80, 80));
+    static Material matEmitBrightW = Material(Color(0, 0, 0), 0, Color(40, 40, 40));
     static Material matDiffuseWhite = Material(Color(0.8, 0.8, 0.8), 1);
     static Material matGlass = Material(Color(0.7, 0.7, 0.7), 0, Color(0, 0, 0), Color(0.9, 0.9, 0.9), 1.3, 1);
-    static Material matDiamond = Material(Color(0.9, 0.9, 0.9), 0, Color(0, 0, 0), Color(0.95, 0.95, 0.95), 2.419, 1);
+    static Material matDiamond = Material(Color(0.2, 0.2, 0.2), 0, Color(0, 0, 0), Color(0.9, 0.9, 0.9), 2.419, 1);
     static Material matMirror = Material(Color(0.99, 0.99, 0.99), 0);
     Object *objects[] =
     {
@@ -91,7 +99,7 @@ Object *makeWorld()
         new Plane(Vector3D(0, 1, 0), 5, &matDiffuseWhite),
         new Plane(Vector3D(-1, 0, 0), 20, &matDiffuseWhite),
         new Plane(Vector3D(1, 0, 0), 20, &matDiffuseWhite),
-        //makeLens(Vector3D(-2.5 / 4, 0, -2.5), Vector3D(-1, 0, -4), 0.5, 1, &matGlass),
+        makeLens(Vector3D(-2.5 / 4, 0, -2.5), Vector3D(-1, 0, -4), 0.5, 1, &matGlass),
         makeLensPointedAt(interpolate(0.9, Vector3D(-1, 10, 14), Vector3D(0, 0, -10)), Vector3D(0, -1, -20), 1.2, 2.5, &matDiamond),
     };
     return unionArray(objects, 0, sizeof(objects) / sizeof(objects[0]));
@@ -104,13 +112,13 @@ private:
     static atomic_bool *threadDone;
     static int threadCount;
     static Runnable *head, *tail;
-    static bool didInit;
-    static mutex listMutex;
+    static volatile bool didInit;
+    static mutex listMutex, doingInit;
     Runnable *next, *prev;
     atomic_bool started, finished;
     condition_variable_any finished_cond;
 
-    static void threadFn(atomic_bool * done)
+    static void threadFn(atomic_bool *done)
     {
         listMutex.lock();
         while(head)
@@ -118,9 +126,13 @@ private:
             Runnable *theRunner = head;
             head = head->next;
             if(head)
+            {
                 head->prev = NULL;
+            }
             else
+            {
                 tail = NULL;
+            }
             theRunner->prev = NULL;
             theRunner->next = NULL;
             theRunner->started = true;
@@ -143,8 +155,10 @@ private:
 
     static void doInit()
     {
+        doingInit.lock();
         if(didInit)
         {
+            doingInit.unlock();
             return;
         }
         didInit = true;
@@ -160,6 +174,7 @@ private:
             threads[i] = NULL;
             threadDone[i] = true;
         }
+        doingInit.unlock();
     }
     Runnable(const Runnable &) = delete;
     const Runnable &operator =(const Runnable &) = delete;
@@ -172,9 +187,13 @@ public:
     {
         listMutex.lock();
         if(head)
+        {
             tail->next = this;
+        }
         else
+        {
             head = this;
+        }
         next = NULL;
         prev = tail;
         tail = this;
@@ -199,7 +218,9 @@ public:
     {
         listMutex.lock();
         while(!finished)
+        {
             finished_cond.wait(listMutex);
+        }
         listMutex.unlock();
     }
     virtual ~Runnable()
@@ -208,31 +229,60 @@ public:
         if(started)
         {
             while(!finished)
+            {
                 finished_cond.wait(listMutex);
+            }
         }
         else
         {
             if(next == NULL)
+            {
                 tail = prev;
+            }
             else
+            {
                 next->prev = prev;
+            }
             if(prev == NULL)
+            {
                 head = next;
+            }
             else
+            {
                 prev->next = next;
+            }
         }
         listMutex.unlock();
+    }
+    static int getThreadCount()
+    {
+        return threadCount;
     }
 };
 
 thread **Runnable::threads = NULL;
 atomic_bool *Runnable::threadDone = NULL;
 int Runnable::threadCount = 0;
-bool Runnable::didInit = false;
+volatile bool Runnable::didInit = false;
 Runnable *Runnable::head = NULL, *Runnable::tail = NULL;
-mutex Runnable::listMutex;
+mutex Runnable::listMutex, Runnable::doingInit;
 
-class RenderBlock final : public Runnable
+class BlockRenderer
+{
+    const BlockRenderer & operator =(const BlockRenderer &) = delete;
+    BlockRenderer(const BlockRenderer &) = delete;
+public:
+    BlockRenderer()
+    {
+    }
+    virtual bool done() = 0;
+    virtual void copyToBuffer(Color *screenBuffer, int w, int h) = 0;
+    virtual ~BlockRenderer()
+    {
+    }
+};
+
+class RenderBlock final : public Runnable, public BlockRenderer
 {
 public:
     RenderBlock(const int x, const int y, int size, const Object *world)
@@ -253,7 +303,7 @@ public:
         join();
         ran_finish = true;
     }
-    bool done()
+    bool done() override
     {
         if(ran_finish)
         {
@@ -265,7 +315,7 @@ public:
         }
         return false;
     }
-    void copyToBuffer(Color *screenBuffer, int w, int h)
+    void copyToBuffer(Color *screenBuffer, int w, int h) override
     {
         for(int y = yOrigin; y < yOrigin + size && y < h; y++)
         {
@@ -277,6 +327,22 @@ public:
                 }
             }
         }
+    }
+    void copyToSocket(FILE *f)
+    {
+        for(int y = yOrigin; y < yOrigin + size; y++)
+        {
+            for(int x = xOrigin; x < xOrigin + size; x++)
+            {
+                if(validPixel(x, y))
+                {
+                    Color v = pixel(x, y);
+                    fprintf(f, "P%i,%i=%g,%g,%g\n", x, y, v.x, v.y, v.z);
+                }
+            }
+        }
+        fprintf(f, "E\n");
+        fflush(f);
     }
     ~RenderBlock()
     {
@@ -399,14 +465,309 @@ private:
     atomic_bool finished;
 };
 
+class NetRenderBlock final : public BlockRenderer
+{
+private:
+    const int x, y, size;
+    Color * const cBuffer;
+    bool * const vBuffer;
+    volatile bool finished = false;
+    mutex bufferMutex;
+    thread * th;
+    static vector<string> addresses;
+    static void threadFn(NetRenderBlock * nrb)
+    {
+        while(!nrb->run())
+        {
+            this_thread::sleep_for(chrono::seconds(1));
+        }
+        nrb->finished = true;
+    }
+    bool run()
+    {
+        string address = addresses[rand() % addresses.size()];
+        addrinfo hints;
+        memset(&hints, 0, sizeof(hints));
+        hints.ai_family = AF_UNSPEC;
+        hints.ai_socktype = SOCK_STREAM;
+        hints.ai_flags = 0;
+        hints.ai_protocol = 0;
+        addrinfo * addrList;
+        int retval = getaddrinfo(address.c_str(), NET_PORT, &hints, &addrList);
+        if (0 != retval)
+        {
+            fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(retval));
+            return false;
+        }
+        int fd = -1;
+        for(addrinfo * addr = addrList; addr; addr = addr->ai_next)
+        {
+            fd = socket(addr->ai_family, addr->ai_socktype, addr->ai_protocol);
+            if(fd == -1)
+                continue;
+            if(-1 != connect(fd, addr->ai_addr, addr->ai_addrlen))
+                break;
+            close(fd);
+            fd = -1;
+        }
+        if(fd == -1)
+        {
+            perror("can't connect");
+            freeaddrinfo(addrList);
+            return false;
+        }
+        freeaddrinfo(addrList);
+        FILE * rdF = fdopen(dup(fd), "r");
+        FILE * wrF = fdopen(fd, "w");
+        int good = fgetc(rdF);
+        if(good != '1')
+        {
+            fclose(rdF);
+            fclose(wrF);
+            return false;
+        }
+        fprintf(wrF, "%i %i %i\n", x, y, size);
+        if(ferror(wrF))
+        {
+            fclose(rdF);
+            fclose(wrF);
+            return false;
+        }
+        fclose(wrF);
+        while(true)
+        {
+            switch(fgetc(rdF))
+            {
+            case 'S':
+            case 'E':
+                break;
+            case '\n':
+                break;
+            case 'P':
+            {
+                int px, py;
+                Color c;
+                if(5 != fscanf(rdF, "%i,%i=%f,%f,%f", &px, &py, &c.x, &c.y, &c.z) || px < x || py < y || px >= x + size || py >= y + size)
+                {
+                    fclose(rdF);
+                    return false;
+                }
+                px -= x;
+                py -= y;
+                bufferMutex.lock();
+                vBuffer[px + size * py] = true;
+                cBuffer[px + size * py] = c;
+                bufferMutex.unlock();
+                break;
+            }
+            case 'F':
+                fclose(rdF);
+                return true;
+            default:
+                fclose(rdF);
+                return false;
+            }
+        }
+    }
+public:
+    NetRenderBlock(int x, int y, int size)
+        : x(x), y(y), size(size), cBuffer(new Color[size * size]), vBuffer(new bool[size * size])
+    {
+        for(int i = 0; i < size * size; i++)
+        {
+            vBuffer[i] = false;
+        }
+        th = nullptr;
+#if 0
+        threadFn(this);
+#else
+        th = new thread(threadFn, this);
+#endif
+    }
+    bool done() override
+    {
+        return finished;
+    }
+    void copyToBuffer(Color *screenBuffer, int w, int h) override
+    {
+        bufferMutex.lock();
+        for(int py = y; py < y + size && py < h; py++)
+        {
+            for(int px = x; px < x + size && px < w; px++)
+            {
+                int rx = px - x, ry = py - y;
+                if(vBuffer[rx + size * ry])
+                {
+                    screenBuffer[px + w * py] = cBuffer[rx + size * ry];
+                }
+            }
+        }
+        bufferMutex.unlock();
+    }
+    ~NetRenderBlock()
+    {
+        th->join();
+        delete th;
+        delete []cBuffer;
+        delete []vBuffer;
+    }
+    friend void client(vector<string>);
+};
+
+vector<string> NetRenderBlock::addresses;
+
+Object *const world = makeWorld();
+AutoDestruct<Object> autoDestruct1(world);
+
+void serverThreadFn(int fd)
+{
+    static atomic_int running_count(0);
+    FILE *f2 = fdopen(dup(fd), "r");
+    FILE *f = fdopen(fd, "w");
+    if(running_count.fetch_add(1) >= Runnable::getThreadCount() * 2)
+    {
+        running_count--;
+        fprintf(f, "0\n");
+        fclose(f2);
+        fclose(f);
+        return;
+    }
+    fprintf(f, "1\n");
+    fflush(f);
+    int x, y, size;
+    if(3 != fscanf(f2, " %i %i %i", &x, &y, &size) || x < 0 || y < 0 || size <= 0 || size > 1000000 || x > ScreenWidth || y > ScreenHeight || size > ScreenWidth || size > ScreenHeight)
+    {
+        running_count--;
+        fclose(f2);
+        fclose(f);
+        return;
+    }
+    cout << "Started Block (" << x << ", " << y << ") " << size << endl;
+    fclose(f2);
+    fprintf(f, "S\n");
+    fflush(f);
+    RenderBlock *rb = new RenderBlock(x, y, size, world);
+    while(!rb->done())
+    {
+        rb->copyToSocket(f);
+        std::this_thread::sleep_for(std::chrono::seconds(1));
+    }
+    rb->copyToSocket(f);
+    fprintf(f, "F\n");
+    fclose(f);
+    delete rb;
+    cout << "Finished Block (" << x << ", " << y << ") " << size << endl;
+    running_count--;
+}
+
+int server()
+{
+    addrinfo hints;
+    memset((void *)&hints, 0, sizeof(hints));
+    hints.ai_family = AF_UNSPEC;
+    hints.ai_socktype = SOCK_STREAM;
+    hints.ai_protocol = 0;
+    hints.ai_flags = AI_PASSIVE | AI_ADDRCONFIG;
+    addrinfo *addrList;
+    if(0 != getaddrinfo(NULL, NET_PORT, &hints, &addrList))
+    {
+        perror("getaddrinfo");
+        exit(EXIT_FAILURE);
+    }
+    int fd = -1;
+    const char *errorStr = "getaddrinfo";
+    for(addrinfo *addr = addrList; addr != nullptr; addr = addr->ai_next)
+    {
+        fd = socket(addr->ai_family, addr->ai_socktype, addr->ai_protocol);
+        errorStr = "socket";
+        if(fd < 0)
+        {
+            continue;
+        }
+        int opt = 1;
+        setsockopt(fd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(int));
+        if(bind(fd, addr->ai_addr, addr->ai_addrlen) == 0)
+        {
+            break;
+        }
+        int temp = errno;
+        close(fd);
+        errno = temp;
+        errorStr = "bind";
+        fd = -1;
+    }
+    if(fd < 0)
+    {
+        perror(errorStr);
+        exit(EXIT_FAILURE);
+    }
+
+    if(listen(fd, 50) == -1)
+    {
+        perror("listen");
+        exit(EXIT_FAILURE);
+    }
+
+    for(int fd2 = accept(fd, NULL, NULL); fd2 >= 0; fd2 = accept(fd, NULL, NULL))
+    {
+        thread(serverThreadFn, fd2).detach();
+    }
+
+    perror("accept");
+    exit(EXIT_FAILURE);
+    return EXIT_FAILURE;
+}
+
+bool isNetworkClient = false;
+
+BlockRenderer * makeBlockRenderer(int x, int y, int size)
+{
+    if(isNetworkClient)
+        return new NetRenderBlock(x, y, size);
+    return new RenderBlock(x, y, size, world);
+}
+
+void client(vector<string> addresses)
+{
+    NetRenderBlock::addresses = addresses;
+    isNetworkClient = true;
+}
+
 int main(int argc, char **argv)
 {
+    signal(SIGPIPE, SIG_IGN);
     if(argc == 2)
     {
         if(argv[1] == string("-h") || argv[1] == string("--help"))
         {
-            cout << "usage: path-trace [--novideo]\n";
+            cout << "usage: path-trace [--novideo] [--server] [--client server.web.address]\n";
             return EXIT_SUCCESS;
+        }
+        else if(argv[1] == string("--server"))
+        {
+            return server();
+        }
+        else
+        {
+            cout << "usage: path-trace [--novideo] [--server] [--client server.web.address]\n";
+            return EXIT_FAILURE;
+        }
+    }
+    else if(argc >= 3)
+    {
+        if(argv[1] == string("--client"))
+        {
+            vector<string> addresses;
+            for(int i = 2; i < argc; i++)
+            {
+                addresses.push_back(string(argv[i]));
+            }
+            client(addresses);
+        }
+        else
+        {
+            cout << "usage: path-trace [--novideo] [--server] [--client server.web.address]\n";
+            return EXIT_FAILURE;
         }
     }
     bool useVideo = true;
@@ -464,9 +825,7 @@ int main(int argc, char **argv)
         SDL_Flip(screen);
         SDL_WM_SetCaption(ProgramName, NULL);
     }
-    Object *const world = makeWorld();
-    AutoDestruct<Object> autoDestruct1(world);
-    RenderBlock *renderers[rendererCount];
+    BlockRenderer *renderers[rendererCount];
     bool renderedAllBlocks = true;
     while(!done)
     {
@@ -520,7 +879,7 @@ int main(int argc, char **argv)
                 {
                     if(anyLeft)
                     {
-                        renderers[i] = new RenderBlock(tempX, tempY, blockSize, world);
+                        renderers[i] = makeBlockRenderer(tempX, tempY, blockSize);
                         tempX += blockSize;
                         if(tempX >= ScreenWidth)
                         {
@@ -592,13 +951,6 @@ int main(int argc, char **argv)
                     }
                     SDL_Delay(100);
                     continue;
-                }
-            }
-            for(int i = 0; i < rendererCount; i++)
-            {
-                if(renderers[i] != nullptr)
-                {
-                    renderers[i]->finish();
                 }
             }
             while(SDL_LockSurface(screen) != 0)
